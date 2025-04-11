0e3e01692e26a83855a7fe1576254389dc469833
JeremieJones507 <jjones8340@conestogac.on.ca>
2025-04-11 23:03:17 +0000

    Trying again

--- snakegame3.c
+++ snakegame3.c
@@ -6,6 +6,7 @@
 #include <time.h>
 #include <stdbool.h>
 #include <conio.h> // Wont work on github
+#include <unistd.h>
 
 // Defining constants
 #define row 6
@@ -24,9 +25,9 @@
 int game_map[row][col]; // The array is defined as a global variable to permit the functions to eddit it directly
 int (*snake_body)[2]; // The array that stores the snakes body as a series of cordinates
 int body_len = START_SIZE - 1; // Length of the snake at the start and will store that attribute for later use minus the head
-int food_pos[2]; // Buffer that hold the food position
+int food_pos[2]; // Bufferss that hold the food position
 bool game_run = true; // Makes the game run and is the end condition for the main loop
-int cmd; // Records user input (Only changes from proper input)
+int cmd = RIGHT; // Records user input (Only changes from proper input)
 // Pause is unimplemented and may never be depends if you want it
 
 // Embedded function decleration
@@ -60,60 +61,53 @@ int main() {
     //printf("%i in %d int memspaces\n", body_len, body_len * sizeof(snake_body)/sizeof(int));
 
     initGame();
-    AddGameObjects();
-    printArray();
-    reset_map();
 
     while (game_run == true && cmd != 'q') { // Main logic loop for the game
-        if (_kbhit()) { // User input loop
-            input();
+        input();
+        if (cmd != 'q') {
             initGameLogic();
             AddGameObjects();
+            system("cls");
             printArray();
-            printf("Food position: [%d, %d]\n", food_pos[0], food_pos[1]); //testing
-
+            //printf("Food position: [%d, %d]\n", food_pos[0], food_pos[1]); //testing
             reset_map();
-            
+            usleep(50000);
         }
     }
 }
 
 // Embedded function definitions
-void initGame() 
-{ // Initializes the game
+void initGame() { // Initializes the game
     initArray();
     append_element(random_pos(), 0); // Sets the head position
-    append_element(random_pos(), body_len);
-    append_element(random_pos(), body_len);
-    append_element(random_pos(), body_len);
+    AddGameObjects();
+    printArray();
+    reset_map();
 
     /* ARBAB CODE HERE*/
     // Initialize food position safely
     bool valid = false;
-    while (!valid) 
-    {
+    while (!valid) {
         int* rand_food = random_pos();
         valid = true;
 
         for (int i = 0; i < body_len; i++) {
-            if (snake_body[i][0] == rand_food[0] && snake_body[i][1] == rand_food[1]) 
-            {
+            if (snake_body[i][0] == rand_food[0] && snake_body[i][1] == rand_food[1]) {
                 valid = false; // food is inside the snake, try again
                 break;
             }
         }
 
-        if (valid) 
-        {
+        if (valid) {
             food_pos[0] = rand_food[0];
             food_pos[1] = rand_food[1];
-            printf("[DEBUG] Food initialized at [%d, %d]\n", food_pos[0], food_pos[1]);
+            //printf("[DEBUG] Food initialized at [%d, %d]\n", food_pos[0], food_pos[1]);
 
         }
 
         free(rand_food);
     }   
-    printf("[DEBUG] Food initialized at [%d, %d]\n", food_pos[0], food_pos[1]);
+    //printf("[DEBUG] Food initialized at [%d, %d]\n", food_pos[0], food_pos[1]);
 
 }
 
@@ -141,20 +135,19 @@ void initGameLogic() {
     foodLogic();
 }
 
-int* move_snake_head(int direction) 
-{
-    printf("I AM ALIVE: %c", direction);
+int* move_snake_head(int direction) {
+    //printf("I AM ALIVE: %c", direction);
     // Allocate memory to store the new head position as [row, col]
     int* new_head_pos = (int*)malloc(2 * sizeof(int));
-    if (new_head_pos == NULL) 
-    {
+
+    if (new_head_pos == NULL) {
         printf("Memory allocation failed!\n");
         exit(1); // Exit the program if memory allocation fails
     }
 
     // Start by copying the current head position
-    printf("|%d|", new_head_pos[0]);
-    printf("|%d|\n", new_head_pos[1]);
+    //printf("|%d|", new_head_pos[0]);
+    //printf("|%d|\n", new_head_pos[1]);
     // Update the position based on the direction input
     switch(direction) {
         case UP:
@@ -179,32 +172,33 @@ int* move_snake_head(int direction)
             break;
     }
 
-    printf("|%d|", new_head_pos[0]);
-    printf("|%d|\n", new_head_pos[1]);
+    //printf("|%d|", new_head_pos[0]);
+    //printf("|%d|\n", new_head_pos[1]);
 
     return new_head_pos;
 
 }
 
-void move_snake(int* new_head_pos) 
-{ // Code for body movement (including head) Body will follow the head
+void move_snake(int* new_head_pos) { // Code for body movement (including head) Body will follow the head
 
-    printf("I DO SOMETHING\n");
+    //printf("I DO SOMETHING\n");
     // e.g. [0, 2], [0, 1], [0, 0] => [0, 3], [0, 2], [0, 1] (With a vertical movement head is the first space)
 
     for (int i = body_len - 1; i > -1; i--) { // Loops through the bidy array
         switch(i) { // To differentiate the head from the rest of the body
             case 0:
-                printf("\n|%d, %d|!", snake_body[0][0], snake_body[0][1]);
+                //printf("\n|%d, %d|!", snake_body[0][0], snake_body[0][1]);
+                //*snake_body[0] = *new_head_pos;
                 snake_body[0][0] = new_head_pos[0]; // Sets the new head position to body segment 0
                 snake_body[0][1] = new_head_pos[1]; // Sets the new head position to body segment 0
-                printf("\n|%d, %d|!", snake_body[0][0], snake_body[0][1]);
+                //printf("\n|%d, %d|!", snake_body[0][0], snake_body[0][1]);
                 break;
             default:
-                printf("\n|%d, %d|: %i", snake_body[i][0], snake_body[i][1], i);
+                //printf("\n|%d, %d|: %i", snake_body[i][0], snake_body[i][1], i);
+                //*snake_body[i] = *snake_body[i - 1];
                 snake_body[i][0] = snake_body[i - 1][0]; // Sets the value of the later segment to the previous position
                 snake_body[i][1] = snake_body[i - 1][1]; // Sets the value of the later segment to the previous position
-                printf("\n|%d, %d|: %i", snake_body[i][0], snake_body[i][1], i);
+                //printf("\n|%d, %d|: %i", snake_body[i][0], snake_body[i][1], i);
                 // May require a error hadeling to the AddGameObject function relating to empty positions if there are more than 1 body segments to fill
                 // e.g. if we have the snake start with more than 1 segment
                 break;
@@ -215,7 +209,7 @@ void move_snake(int* new_head_pos)
 
 void AddGameObjects() { // Adds snake body to array for print (food will be inlucded later)
 
-    game_map[food_pos[0]][food_pos[1]] = '&'; // The food
+    game_map[food_pos[0]][food_pos[1]] = '*'; // The food
     for (int i = body_len - 1; i > -1; i--) {
         //printf("%i: [%d, %d]\n", i, snake_body[i][0], snake_body[i][1]);
         switch (i) {
@@ -247,7 +241,7 @@ void printArray() { // Function that prints the array out in the console
     }
     printf("\n");
 
-    printf("Food map char at [%d, %d] = %c\n", food_pos[0], food_pos[1], game_map[food_pos[0]][food_pos[1]]);
+    //printf("Food map char at [%d, %d] = %c\n", food_pos[0], food_pos[1], game_map[food_pos[0]][food_pos[1]]);
 
 }
 
@@ -273,12 +267,13 @@ void append_element(int* append_element, int append_pos) { // Function that appe
 }
 
 void input() { // Records user input (Uniplemented as of yet)
+    if (_kbhit()) { // User input loop
+        int pressedKey = getch(); // Fetches the pressed keys' value
 
-    int pressedKey = getch(); // Fetches the pressed keys' value
-
-    if (pressedKey == UP || pressedKey == LEFT || pressedKey == DOWN || pressedKey == RIGHT || pressedKey == PAUSE || pressedKey == QUIT) { // Checks if the entered key is assigned to a command
-        cmd = pressedKey;
-        printf("%c", cmd);
+        if (pressedKey == UP || pressedKey == LEFT || pressedKey == DOWN || pressedKey == RIGHT || pressedKey == PAUSE || pressedKey == QUIT) { // Checks if the entered key is assigned to a command
+            cmd = pressedKey;
+            printf("%c", cmd);
+        }
     }
 }
 
@@ -286,11 +281,9 @@ void input() { // Records user input (Uniplemented as of yet)
 Arbab code again 
 */
 
-void foodLogic() 
-{
+void foodLogic() {
     // Check if snake head is at the same position as food
-    if (snake_body[0][0] == food_pos[0] && snake_body[0][1] == food_pos[1]) 
-    {
+    if (snake_body[0][0] == food_pos[0] && snake_body[0][1] == food_pos[1]) {
         // Grow the snake (duplicate tail segment)
         int* new_segment = (int*)malloc(2 * sizeof(int));
         new_segment[0] = snake_body[body_len - 1][0];
@@ -304,16 +297,14 @@ void foodLogic()
             int* rand_food = random_pos();
             valid = true;
 
-            for (int i = 0; i < body_len; i++) 
-            {
+            for (int i = 0; i < body_len; i++) {
                 if (snake_body[i][0] == rand_food[0] && snake_body[i][1] == rand_food[1]) {
                     valid = false;
                     break;
                 }
             }
 
-            if (valid) 
-            {
+            if (valid) {
                 food_pos[0] = rand_food[0];
                 food_pos[1] = rand_food[1];
             }
@@ -322,9 +313,6 @@ void foodLogic()
         }
     }
 
-    // Always make sure the food is shown on the map
-    game_map[food_pos[0]][food_pos[1]] = '*';
-
-    printf("[DEBUG] Food logic ran. Food at: [%d, %d]\n", food_pos[0], food_pos[1]);
+    //printf("[DEBUG] Food logic ran. Food at: [%d, %d]\n", food_pos[0], food_pos[1]);
 
 }
